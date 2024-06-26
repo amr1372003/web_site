460133f93667acbe8f62dda4ea3d69a695214128
amr1372003 <70038874+amr1372003@users.noreply.github.com>
2024-06-27 01:39:19 +0300

    Add files via upload

--- front/script.js
+++ front/script.js
@@ -1,12 +1,26 @@
 const toggle = document.querySelector('.toggle');
 const navItems = document.querySelectorAll('.nav-item');
+const menu = document.querySelector('.menu');
 
-toggle.addEventListener('click', () => {
+let isThrottled = false;
+
+const toggleMenu = () => {
+  if (isThrottled) return;
+  isThrottled = true;
+  setTimeout(() => isThrottled = false, 300);
+
   navItems.forEach(item => item.classList.toggle('active'));
+};
+
+toggle.addEventListener('click', toggleMenu);
+
+menu.addEventListener('click', (event) => {
+  if (event.target.classList.contains('nav-item')) {
+    navItems.forEach(item => item.classList.remove('active'));
+  }
 });
 
-navItems.forEach(item => {
-  item.addEventListener('click', () => {
-    navItems.forEach(item => item.classList.remove('active'));
-  });
-});
+
+/*new*/
+
+/*end of new*/
